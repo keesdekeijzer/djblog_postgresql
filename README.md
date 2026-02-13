9d32d3da7ee463aa26d9bfadeaaad03d5c51397f
keesdekeijzer <git_kdk@xpmail.nl>
2026-02-13 19:25:34 +0100

    Add blog application with models, views, templates, and static files
    - Created models for Post and Comment with custom managers and fields.
    - Implemented views for listing posts, post details, and pagination.
    - Added templates for base layout, post listing, detail view, and comments.
    - Integrated tag management using django-taggit.
    - Set up sitemaps for SEO optimization.
    - Created custom template tags for displaying total posts and latest posts.
    - Added CSS for styling the blog interface.
    - Configured URLs for the blog application and integrated with the main project.
    - Implemented WSGI and ASGI configurations for deployment.
    - Added initial tests for the blog application.

---
+++ README.md
@@ -0,0 +1,29 @@
+# DJBLOG
+
+Een basis voor een Django blog website.
+
+## Development server starten
+
+```
+python manage.py runserver
+```
+
+Te zien op http://127.0.0.1:8000/
+
+## Make migrations
+
+```
+python manage.py makemigrations blog
+```
+
+## Apply migrations
+
+```
+python manage.py migrate
+```
+
+## Creating a superuser
+
+```
+python manage.py createsuperuser
+```
---
+++ mysite/blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from taggit.models import Tag
+from django.db.models import Count
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 2)  # Show 2 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    comment = None
+    # Comment posted
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+    else:
+        form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                      'comments': comments,
+                                                        'new_comment': comment,
+                                                          'comment_form': form,
+                                                            'similar_posts': similar_posts})
+
